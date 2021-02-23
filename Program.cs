8430e650084704559cfbfb7046be67467eb4089f
Charlie <charlesrichardson123000@gmail.com>
2021-02-23 15:53:48 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace document_merger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

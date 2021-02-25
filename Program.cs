af7d4226094a601262dccc67563b255ea0c0607a
Charlie <charlesrichardson123000@gmail.com>
2021-02-25 16:51:18 -0600

    Final

--- Program.cs
+++ Program.cs
@@ -1,66 +1,137 @@
 ﻿using System;
 using System.IO; 
-
+//make all arrays 
 namespace document_merger
 {
-    class Program
-    {
-        public class document_merger{
-            public string content1;
-            public string content2;
-            public string document1;
-            public string document2;
-            public document_merger(){
+    public class merger
+        {
+            public string content1,content2,document1,document2,mergedContent,mergedName,ErrorCode;
+            public merger()
+            {
                  document1=getFileName(1);
                  document2=getFileName(2);
                  content1=scanfile(document1);
                  content2=scanfile(document2);
-                
+                 mergedName=rename(document1,document2);
+                 mergedContent+=content1+content2;
+                 ErrorCode=merge(mergedName,mergedContent);
             }
-            public void merge(){
-                string content3;
-                content3=content1+content2;
-                
+            public string merge(string  mergedName,string  mergedContent)
+            {
+                try{
+                StreamWriter sr= new StreamWriter(mergedName);
+                sr.WriteLine(mergedContent);
+                sr.Close();
+                return($"{mergedName} was successfully saved. The document contains {mergedContent.Length} characters");
+                }
+                catch(Exception e)
+                {
+                    return(e.Message);
+                }
             }
-            public string scanfile(string WD){
-
+            public string scanfile(string WD)
+            {
+                int len=WD.Length;
+                if(!(WD[len-1]=='t' && WD[len-2]=='x' &&WD[len-3]=='t'&&WD[len-4]=='.'))
+                {
+                            WD+=".txt";
+                        }
+                string content;
+                StreamReader sr = new StreamReader(WD);
+                content=sr.ReadToEnd();
+                sr.Close();
+                return content;
             }
-            public string getFileName(int tryNumber){
+            public string getFileName(int tryNumber)
+            {
                 string name;
-                do{
+                while(true)
+                {
                     Console.WriteLine($"Please enter name of ducement{tryNumber}:");
                     name=Console.ReadLine();
-                    int len=name.Length;
-                    if(name[len-1]=='t' && name[len-2]=='x' &&name[len-3]=='t'&&name[len-4]=='.'){
-                        name=@"\"+name;
+                    if(name==""||name==".txt"){
+                        Console.WriteLine("File name cannot be blank.");
+                        continue;
                     }
-                }while(isInDirectory(name));
-            }
-            public string rename(){
-
-            }
-            public Boolean isInDirectory(string name){
-                
-            }
-            public 
-
-        }
-        static void Main(string[] args)
-        { 
-            string content="",document1="",document2 = "",WD=@"\";
-            Console.WriteLine("Document Merger\n");
-            do{
-                while(true){
-                    try{ Console.WriteLine("Please input first document to merge.");
-                    
+                    Boolean bl=isInDirectory(name);
+                    if(bl)
+                    {
+                        int len=name.Length;
+                        break;
                     }
-                    catch{
-
+                    else
+                    {
+                        continue;
                     }
                 }
-            }while(true);
-            document_merger merge=new document_merger();
-
+                return name.Replace(".txt","");
+            }
+            public string rename(string name1,string name2)
+            {
+                while(true){
+                    string name;
+                    Console.WriteLine($"What would you like the string to be called?\nfor default,({name1+name2+".txt"}), input -1:");
+                    name=Console.ReadLine();
+                    if(name==""||name==".txt"){
+                            Console.WriteLine("File name cannot be blank.");
+                            continue;
+                        }
+                    if(name!="-1")
+                    {
+                        int len=name.Length;
+                        if(!(name[len-1]=='t' && name[len-2]=='x' &&name[len-3]=='t'&&name[len-4]=='.')){
+                            name+=".txt";
+                        }
+                        return name;
+                    }
+                    else
+                    {
+                        return(name1+name2+".txt");
+                    }
+                }
+            }
+            public bool isInDirectory(string name)
+            {
+                try
+                {
+                    int len=name.Length;
+                    if(!(name[len-1]=='t' && name[len-2]=='x' &&name[len-3]=='t'&&name[len-4]=='.'))
+                {
+                            name+=".txt";
+                        }
+                    StreamReader sr = new StreamReader(name);
+                    if(sr==null){
+                        Console.WriteLine("File is empty or missing.");
+                        return false;
+                    }
+                    sr.Close();
+                    return true;
+                }
+                catch(FileNotFoundException)
+                {
+                    Console.WriteLine("Cannot find file.");
+                    return false;
+                }    
+            }
         }
+    class Program
+    {       
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Document Merger\n");
+            while(true){
+                merger merge=new merger();
+                Console.WriteLine(merge.ErrorCode+" Input y to do it again or enter any other character to exit.");
+                string input=Console.ReadLine();
+                if(input=="y"){
+                    continue;
+                }
+                else{
+                    break;
+                }
+                
+            }
+        }
+
     }
 }
